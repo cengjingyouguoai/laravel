f7093b06fa38beb045901d78681ce8376765f65b
wenliguo <wenliguo@17shanyuan.com>
2018-04-12 17:24:51 +0800

    路由

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//大后台登录
+Route::group(['namespace' => 'Admin\Login'],function(){
+    //登录页面
+    Route::get('admin/login/login', 'LoginController@login');
+
+    //登录判断
+    //Route::post('admin/login_deal','LoginController@loginDeal');
+
+    //后台退出
+    //Route::get('admin/login_out','LoginController@loginOut');
+
+});
+//后台首页
+Route::group(['namespace' => 'Admin\Index'],function(){
+    //后台首页
+    Route::get('admin/index/index','IndexController@index');
+    //后台首页2
+    Route::get('admin/index/index_2','IndexController@indexTwo');
+
+
+});
