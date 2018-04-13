068671d2158f1cd60f21d8aba3a2add1fe63cac7
wenliguo <wenliguo@17shanyuan.com>
2018-04-13 12:07:58 +0800

    登录

--- blog/app/Http/Controllers/Admin/Login/LoginController.php
+++ blog/app/Http/Controllers/Admin/Login/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin\Login;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Model\Admin;
 class LoginController extends Controller
 {
     /**
@@ -13,5 +13,33 @@ class LoginController extends Controller
     public function login()
     {
         return view('admin.login.login');
+    }
+
+    /**
+     * 退出
+     */
+    public function loginOut(Request $request)
+    {
+        $request->session()->forget('username');
+        if (!$request->session()->exists('username')) {
+            return  successJump('admin/lgoin/login');
+        }
+    }
+
+    /**
+     * 登录判断
+     */
+    public function loginDeal(Request $request)
+    {
+        $username = trim($request->input('username'));//用户名
+        $password = trim($request->input('password'));//密码
+        $adminModel = new Admin;
+        $result = $adminModel->checkLogin($username,md5($password));
+        if ($result) {
+            $request->session()->put('username',$username);
+            return  successJump('admin/index/index','登录成功');
+        } else {
+            return  errorJump('admin/login/login','登录失败');
+        }
     }
 }
