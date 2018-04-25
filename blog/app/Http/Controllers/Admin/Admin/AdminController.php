9b82f332b693c97b2a8edf104ed90b97b8dd2dcb
wenliguo <wenliguo@17shanyuan.com>
2018-04-25 10:10:41 +0800

    完善

--- blog/app/Http/Controllers/Admin/Admin/AdminController.php
+++ blog/app/Http/Controllers/Admin/Admin/AdminController.php
@@ -14,6 +14,37 @@ class AdminController extends Controller
     {
         $adminModel = new Admin();
         $data = $adminModel->getData();
-        return view('admin/admin/admin_list',['data' => $data]);
+        return view('admin.admin.admin_list',['data' => $data]);
+    }
+    /**
+     * 管理员修改
+     */
+    public function adminUpdate(Request $request)
+    {
+        $id = intval($request->input('id'));
+        $adminModel = new Admin();
+        $data = $adminModel->getOneData($id);
+        return view('admin.admin.admin_update',['data' => $data]);
+    }
+
+    /**
+     * 管理员修改处理
+     */
+    public function adminUpdateDeal(Request $request)
+    {
+        $id = intval($request->input('id'));
+        $passowrd = trim($request->input('password'));
+        $passowrd = md5($passowrd);
+        $adminModel = new Admin();
+        $data = [
+            'admin_password' => $passowrd,
+            'update_time'  => time()
+        ];
+        $result = $adminModel->updateData($data,$id);
+        if ($result) {
+            return successJump('admin/admin/admin_list','修改成功');
+        } else {
+            return errorJump('admin/admin/admin_list','修改失败');
+        }
     }
 }
