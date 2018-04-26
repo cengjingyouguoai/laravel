9eb5ebdc7e5fc292faebd72fdf3a09d069e6b1e8
wenliguo <wenliguo@17shanyuan.com>
2018-04-26 09:43:31 +0800

    公共部分

---
+++ blog/resources/views/home/common/common_css.blade.php
@@ -0,0 +1,2 @@
+<link href="{{ URL::asset('home/css/styles.css') }}" rel="stylesheet">
+<link href="{{ URL::asset('home/css/animation.css') }}" rel="stylesheet">
---
+++ blog/resources/views/home/common/common_js.blade.php
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="{{ URL::asset('home/js/jquery.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('home/js/js.js') }}"></script>
+<!-- 返回顶部调用 end-->
+<!--[if lt IE 9]>
+    <script src="{{ URL::asset('home/js/modernizr.js')}}"></script>
