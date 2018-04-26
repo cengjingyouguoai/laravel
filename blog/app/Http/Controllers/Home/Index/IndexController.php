1510a0a10e5ed541027d8fbad9cea0354869abb9
wenliguo <wenliguo@17shanyuan.com>
2018-04-26 17:49:22 +0800

    完善

--- blog/app/Http/Controllers/Home/Index/IndexController.php
+++ blog/app/Http/Controllers/Home/Index/IndexController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\Type;
 use App\Model\Title;
+use App\Model\Article;
 class IndexController extends Controller
 {
     /**
@@ -17,7 +18,9 @@ class IndexController extends Controller
         $typeData = $typeModel->getTypeList();//首页分类
         $titleModel = new Title();
         $titleData = $titleModel->getOneData();
-        return view('home.index.index',['type_data' => $typeData,'title_data' => $titleData]);
+        $articleModel = new Article();
+        $articleData = $articleModel->getArticleList();
+        return view('home.index.index',['type_data' => $typeData,'title_data' => $titleData,'article_data' => $articleData]);
     }
 
 }
