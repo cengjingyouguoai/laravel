643b6a8013041019d8973dda8f1b6f0811f05f6b
wenliguo <wenliguo@17shanyuan.com>
2018-04-27 19:08:36 +0800

    完善

--- blog/app/Model/Comment.php
+++ blog/app/Model/Comment.php
@@ -23,6 +23,15 @@ class Comment extends Model
      */
     public function getCommentByArticleId($articleId)
     {
+       /* $result =  Comment::leftjoin('recomment','comment.recomment_id','=','recomment.id')
+            ->select('comment.id','comment.article_id','comment.comment_content','recomment.create_at','recomment.id','recomment.recomment_content','recomment.create_at as creates_at')
+            ->orderBy('recomment.create_at','desc')
+            ->get();
+        if ($result) {
+            return $result->toArray();
+        } else {
+            return $result;
+        }*/
         $result = Comment::where(['article_id' => $articleId])->orderBy('create_at','desc')->limit(10)->get();
         if ($result) {
             return $result->toArray();
