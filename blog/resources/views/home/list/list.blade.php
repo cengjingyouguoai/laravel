eaba7ba6b9e856f74e36f1797fae94ca2882152f
wenliguo <wenliguo@17shanyuan.com>
2018-04-27 10:42:11 +0800

    完善

--- blog/resources/views/home/list/list.blade.php
+++ blog/resources/views/home/list/list.blade.php
@@ -51,42 +51,30 @@
             <div class="tuijian">
                 <h2>推荐文章</h2>
                 <ol>
-                    <li><span><strong>1</strong></span><a href="/">有一种思念，是淡淡的幸福,一个心情一行文字</a></li>
-                    <li><span><strong>2</strong></span><a href="/">励志人生-要做一个潇洒的女人</a></li>
-                    <li><span><strong>3</strong></span><a href="/">女孩都有浪漫的小情怀——浪漫的求婚词</a></li>
-                    <li><span><strong>4</strong></span><a href="/">Green绿色小清新的夏天-个人博客模板</a></li>
-                    <li><span><strong>5</strong></span><a href="/">女生清新个人博客网站模板</a></li>
-                    <li><span><strong>6</strong></span><a href="/">Wedding-婚礼主题、情人节网站模板</a></li>
-                    <li><span><strong>7</strong></span><a href="/">Column 三栏布局 个人网站模板</a></li>
-                    <li><span><strong>8</strong></span><a href="/">时间煮雨-个人网站模板</a></li>
-                    <li><span><strong>9</strong></span><a href="/">花气袭人是酒香—个人网站模板</a></li>
+                    @foreach($recommend_data as $key => $val)
+                        <li><span><strong>{{ $key + 1 }}</strong></span><a href="{{ url('article/article_list') }}?article_id={{ $val['article_id'] }}">{{ $val['article_title'] }}</a></li>
+                    @endforeach
                 </ol>
             </div>
 
             <div class="clicks">
                 <h2>热门点击</h2>
                 <ol>
-                    <li><span><a href="/">慢生活</a></span><a href="/">有一种思念，是淡淡的幸福,一个心情一行文字</a></li>
-                    <li><span><a href="/">爱情美文</a></span><a href="/">励志人生-要做一个潇洒的女人</a></li>
-                    <li><span><a href="/">慢生活</a></span><a href="/">女孩都有浪漫的小情怀——浪漫的求婚词</a></li>
-                    <li><span><a href="/">博客模板</a></span><a href="/">Green绿色小清新的夏天-个人博客模板</a></li>
-                    <li><span><a href="/">女生个人博客</a></span><a href="/">女生清新个人博客网站模板</a></li>
-                    <li><span><a href="/">Wedding</a></span><a href="/">Wedding-婚礼主题、情人节网站模板</a></li>
-                    <li><span><a href="/">三栏布局</a></span><a href="/">Column 三栏布局 个人网站模板</a></li>
-                    <li><span><a href="/">个人网站模板</a></span><a href="/">时间煮雨-个人网站模板</a></li>
-                    <li><span><a href="/">古典风格</a></span><a href="/">花气袭人是酒香—个人网站模板</a></li>
+                    @foreach($hot_data as $key => $val)
+                        <li><span><a href="{{ url('article/article_list') }}?article_id={{ $val['article_id'] }}">{{ $val['article_title'] }}</a></span>{{--<a href="/"></a>--}}</li>
+                    @endforeach
                 </ol>
             </div>
 
             <div class="viny">
                 <dl>
-                    <dt class="art"><img src="images/artwork.png" alt="专辑"></dt>
+                    <dt class="art"><img src="{{ URL::asset('home/images/artwork.png') }}" alt="专辑"></dt>
                     <dd class="icon-song"><span></span>南方姑娘</dd>
                     <dd class="icon-artist"><span></span>歌手：赵雷</dd>
                     <dd class="icon-album"><span></span>所属专辑：《赵小雷》</dd>
                     <dd class="icon-like"><span></span><a href="/">喜欢</a></dd>
                     <dd class="music">
-                        <audio src="images/nf.mp3" controls></audio>
+                        <audio src="{{ URL::asset('home/images/nf.mp3') }}" controls></audio>
                     </dd>
                     <!--也可以添加loop属性 音频加载到末尾时，会重新播放-->
                 </dl>
