e017cfd0626e45d55d147b81f3d55f5f42c20e73
wenliguo <wenliguo@17shanyuan.com>
2018-04-27 19:08:46 +0800

    完善

--- blog/app/Model/Recomment.php
+++ blog/app/Model/Recomment.php
@@ -3,8 +3,30 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class Recomment extends Model
 {
-    //
+    public $timestamps = false;
+    protected $table = 'recomment';
+
+    /**
+     * 增加回复
+     */
+    public function addRecomment($data)
+    {
+        return DB::table($this->table)->insert($data);
+    }
+
+    /**
+     * 获取文章回复
+     */
+    public function getRecomment($recommentId)
+    {
+        $result = Recomment::where(['comment_id' => $recommentId])->orderBy('create_at','desc')->limit(10)->get();
+        if ($result) {
+            return $result->toArray();
+        } else {
+            return [];
+        }
+    }
 }
