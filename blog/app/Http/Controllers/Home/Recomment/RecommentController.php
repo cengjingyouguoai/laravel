e5268ce1ac551e933d002dca451554c1c758ed6a
wenliguo <wenliguo@17shanyuan.com>
2018-04-27 16:25:12 +0800

    回复

---
+++ blog/app/Http/Controllers/Home/Recomment/RecommentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Home\Recomment;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RecommentController extends Controller
+{
+    //
+}
