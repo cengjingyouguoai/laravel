d9c5f6454a5c5802df350588a3e8022e3333673a
wenliguo <wenliguo@17shanyuan.com>
2018-05-02 10:13:37 +0800

    首页

--- blog/app/Http/Controllers/Admin/Index/IndexController.php
+++ blog/app/Http/Controllers/Admin/Index/IndexController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Admin\Index;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Model\Article;
+use App\Model\Type;
 class IndexController extends Controller
 {
     /**
@@ -12,7 +13,11 @@ class IndexController extends Controller
      */
     public function index()
     {
-        return view('admin.index.index');
+        $articleModel = new Article();
+        $countArticle = $articleModel->countArticle();
+        $typeModel = new Type();
+        $countType = $typeModel->countType();
+        return view('admin.index.index',['count_article' => $countArticle,'count_type' => $countType]);
     }
 
     /**
